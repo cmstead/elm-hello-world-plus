72e0e3b848c2200661edfa12878e00466feb69ac
Chris Stead <cmstead@gmail.com>
2017-10-06 17:52:33 -0700

    Still working on various refactorings to find happy code style

--- Hello.elm
+++ Hello.elm
@@ -1,7 +1,6 @@
 module Main exposing (..)
 
 import Html exposing (..)
-import Html.Events exposing (onInput)
 import HelloTypes exposing (..)
 import HelloView exposing (..)
 
@@ -15,21 +14,39 @@ main =
         , subscriptions = subscriptions
         }
 
+stringOrDefault : String -> String -> String
+stringOrDefault defaultValue stringValue = 
+    if stringValue == "" then
+        defaultValue
+    else
+        stringValue
+
+defaultName : String
+defaultName = "World"
+
+defaultMessage : String 
+defaultMessage = "Hello!"
 
 update : InputMsg -> ViewModel -> ( ViewModel, Cmd a )
 update inputMessageChange model =
     case inputMessageChange of
         NameChange updatedContent ->
-            ( { model | name = updatedContent }, Cmd.none )
+            let
+                nameValue = (stringOrDefault defaultName updatedContent)
+            in 
+                ( { model | name = nameValue }, Cmd.none )
 
         MessageChange updatedContent ->
-            ( { model | message = updatedContent }, Cmd.none )
+            let 
+                messageValue = (stringOrDefault defaultMessage updatedContent)
+            in 
+                ( { model | message = messageValue }, Cmd.none )
 
 
 init : ( ViewModel, Cmd a )
 init =
-    ( { name = "Dex"
-      , message = "Hello!"
+    ( { name = defaultName
+      , message = defaultMessage
       }
     , Cmd.none
     )
